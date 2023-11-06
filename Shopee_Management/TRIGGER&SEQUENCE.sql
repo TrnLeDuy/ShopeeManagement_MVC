b5bb30be25069e785c1622325cb9ae160845e107
Trần Lê Duy <trn.duyle@gmail.com>
2023-11-06 08:47:41 +0700

    udpate trigger NGUOIBANHANG

--- Shopee_Management/TRIGGER&SEQUENCE.sql
+++ Shopee_Management/TRIGGER&SEQUENCE.sql
@@ -14,10 +14,33 @@ BEGIN
     DECLARE @NewID VARCHAR(255);
     SELECT @NewID = 'KH' + RIGHT('0000000000' + CAST((SELECT ISNULL(MAX(CAST(RIGHT(id_kh, 10) AS INT)), 0) + 1 FROM KHACHHANG) AS VARCHAR), 10);
 
-    INSERT INTO KHACHHANG (id_kh, email, username, [password])
+    INSERT INTO KHACHHANG (id_kh, sdt, email, username, [password])
     SELECT 
         CASE WHEN i.id_kh IS NULL THEN @NewID ELSE i.id_kh END,
-        i.email, i.username, i.[password]
+        i.sdt, i.email, i.username, i.[password]
+    FROM inserted i;
+END;
+
+CREATE SEQUENCE NGUOIBANHANG_SEQUENCE
+    AS INT
+    START WITH 1
+    INCREMENT BY 1
+    MINVALUE 1
+    NO MAXVALUE
+    CYCLE;
+	
+CREATE OR ALTER TRIGGER trgInsertNGUOIBANHANG
+ON NGUOIBANHANG
+INSTEAD OF INSERT
+AS
+BEGIN
+    DECLARE @NewID VARCHAR(255);
+    SELECT @NewID = 'NBH' + RIGHT('0000000000' + CAST((SELECT ISNULL(MAX(CAST(RIGHT(id_nbh, 10) AS INT)), 0) + 1 FROM NGUOIBANHANG) AS VARCHAR), 10);
+
+    INSERT INTO NGUOIBANHANG(id_nbh, ten_cua_hang, trang_thai_ch, id_kh)
+    SELECT 
+        CASE WHEN i.id_nbh IS NULL THEN @NewID ELSE i.id_nbh END,
+        i.ten_cua_hang, i.trang_thai_ch, i.id_kh
     FROM inserted i;
 END;
 
